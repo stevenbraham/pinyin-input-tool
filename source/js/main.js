d4d0a788e5dce490f4d611ced38151c43913ebcb
Steven Braham <steven@braham.biz>
2016-02-24 16:47:50 +0100

    it works\!

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     $("#pinyinText").on("input",function(){
         var originalText = $(this).val();
-        console.log();
+        //search a pinyin tone code using regex
+        var regexMatch = originalText.match(/([a,A,e,E,I,i,o,O,U,u][0,1,2,3,4,5])/);
+        if(regexMatch){
+            var pinYincode = regexMatch[0];
+            var newCharacter = pinyinTable[pinYincode[0]][pinYincode[1]];
+            var newText = originalText.replace(pinYincode,newCharacter);
+            $("#pinyinText").val(newText);
+        }
     })
 });
